eda0a22f08ba5053036899bc85d3cdee01ddc7a7
tmdgusya <tmdgusya@naver.com>
2022-05-03 21:00:48 +0900

    5.1.1 Example01 Aggregate

--- src/main/kotlin/chapter05/high_order_func/Example01.kt
+++ src/main/kotlin/chapter05/high_order_func/Example01.kt
@@ -8,6 +8,30 @@ fun sum(numbers: IntArray): Int {
     return result
 }
 
+fun sumWithAggregate(numbers: IntArray): Int {
+    return aggregate(numbers) { result, op -> result + op }
+}
+
+fun max(numbers: IntArray): Int {
+    return aggregate(numbers) {
+        result, op -> if (op > result) op else result
+    }
+}
+
+fun aggregate(numbers: IntArray, op: (Int, Int) -> Int): Int {
+    val result = numbers.firstOrNull() ?: throw IllegalArgumentException("Empty Array")
+
+    for (i in 1..numbers.lastIndex) op(result, numbers[i])
+
+    return result
+}
+
 fun main() {
-    println(sum(intArrayOf(1, 2, 3, 4, 5)))
+    val given = intArrayOf(1, 2, 3, 4, 5)
+
+    println(sum(given))
+
+    println(sumWithAggregate(given))
+
+    println(max(given))
 }

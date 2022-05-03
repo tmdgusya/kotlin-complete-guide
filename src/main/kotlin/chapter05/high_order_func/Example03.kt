25aacea2fc1523bdb8803162577e7a0cddf17a97
tmdgusya <tmdgusya@naver.com>
2022-05-03 21:42:48 +0900

    5.1.4 Example03

---
+++ src/main/kotlin/chapter05/high_order_func/Example03.kt
@@ -0,0 +1,19 @@
+package chapter05.high_order_func
+
+import java.lang.Character.isUpperCase
+
+fun check(s: String, condition: (Char) -> Boolean): Boolean {
+    for (c in s) {
+        if (!condition(c)) return false
+    }
+    return true
+}
+
+fun main() {
+
+    println(check("hello") {
+        it.isUpperCase()
+    })
+
+    println(check("Hello", ::isUpperCase))
+}

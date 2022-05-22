6ef9c9f75e951220d31f37eba1d3f0eb1d47bdae
tmdgusya <tmdgusya@naver.com>
2022-05-22 21:26:39 +0900

    6.2.1 데이터 클래스와 데이터 클래스에 대한 연산

---
+++ src/main/kotlin/chapter06/Example03.kt
@@ -0,0 +1,31 @@
+package chapter06
+
+class Person(
+    val firstName: String,
+    val familyName: String,
+    val age: Int
+)
+
+data class DataPerson(
+    val firstName: String,
+    val familyName: String,
+    val age: Int
+)
+
+fun main() {
+    val person1 = Person("John", "Doe", 25)
+    val person2 = Person("John", "Doe", 25)
+    val person3 = person1
+
+    println(person1 == person2) // false
+    println(person1 === person3) // true diff address equality
+    println(person1 == person3) // true call `equals`
+
+    val dataPerson1 = DataPerson("John", "Doe", 25)
+    val dataPerson2 = DataPerson("John", "Doe", 25)
+    val dataPerson3 = dataPerson1
+
+    println(dataPerson1 == dataPerson2) // true
+    println(dataPerson1 === dataPerson3) // true diff address equality
+    println(dataPerson1 == dataPerson3) // true call `equals`
+}

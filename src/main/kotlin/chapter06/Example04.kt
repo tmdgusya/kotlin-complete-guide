b6c98be33207a6a6cff9eade6d44323e02398a01
tmdgusya <tmdgusya@naver.com>
2022-05-31 19:48:50 +0900

    immutable downgrade mutable

--- src/main/kotlin/chapter06/Example04.kt
+++ src/main/kotlin/chapter06/Example04.kt
@@ -15,4 +15,20 @@ value class InlineDollar(val amount: Int) {
 fun main() {
     println(InlineDollar(50).add(InlineDollar(20)).amount)
     println(InlineDollar(-100).isDebt)
+    var a: List<Int> = listOf<Int>(1, 2, 3)
+    val c = a
+    val b: MutableList<Int> = mutableListOf(4, 5, 6, 7)
+    a = b.map {
+        it + 1
+    }
+    println(a) // [5, 6, 7, 8]
+    println(c) // [1, 2, 3]
+
+    var test: List<Int> = listOf(1, 2, 3)
+
+    if (test is MutableList<Int>) {
+        test.add(3)
+    }
+
+    println(test)
 }
